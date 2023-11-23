e8a78769fee6712e09b77e2f186e94f8e47973a2
jonakls <asmot54@hotmail.com>
2023-11-23 11:06:02 -0500

    chore(name): rename project

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='radix-organizer',
+    name='radix-format',
     version='0.0.1',
     packages=find_packages(),
     install_requires=[

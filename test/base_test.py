efe121f22d0d93fa6e37431a4817d2c9b71f74ba
jonakls <asmot54@hotmail.com>
2023-12-01 09:03:19 -0500

    refactor(schema): change general format and add test

---
+++ test/base_test.py
@@ -0,0 +1,6 @@
+from radix_format.organizer_format import init_path
+
+init_path(
+    "C:\\Users\\jonakls\\OneDrive - LITIGAR PUNTO COM S.A\\Desarrollos\\FileExtra\\RADICADOS\\TEST_RAD.xlsx",
+    "C:\\Users\\jonakls\\OneDrive - LITIGAR PUNTO COM S.A\\Desarrollos\\FileExtra\\RADICADOS\\TEST_RAD_RESULT.csv"
+)
